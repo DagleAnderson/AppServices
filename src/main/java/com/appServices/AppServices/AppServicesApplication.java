6ece5cce2c350f5b978a468a79bdb928c02897ba
D'aglê Anderson <dagle_life@hotmail.com>
2019-02-15 23:29:01 -0300

    implementação de envio de arquivos para AmazonS3

--- src/main/java/com/appServices/AppServices/AppServicesApplication.java
+++ src/main/java/com/appServices/AppServices/AppServicesApplication.java
@@ -1,14 +1,18 @@
 package com.appServices.AppServices;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.appServices.AppServices.Services.S3service;
 
 
 @SpringBootApplication
 public class AppServicesApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3service s3service;
 	
 	
 	public static void main(String[] args){
@@ -18,6 +22,7 @@ public class AppServicesApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 	
+		s3service.uploadFile("C:\\Users\\dagle\\OneDrive\\Documentos\\IGTI\\TCC\\appservicesCapa.JPG");
 	 	
 	}
 }
