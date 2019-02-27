f546d4c3e8aec5c02f1e8fb45aadaf48a1f964ef
D'aglê Anderson <dagle_life@hotmail.com>
2019-02-26 23:24:17 -0300

    ajuste em aplication.properties

--- src/main/java/com/appServices/AppServices/config/SecurityConfig.java
+++ src/main/java/com/appServices/AppServices/config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	public static final String[] PUBLIC_MATCHERS_GET = {
 			"/h2-console/**",
-			"/prestador/**"
+			"/prestador/**",
+			"/categorias/**"
 	};
 	
 	public static final String[] PUBLIC_MATCHERS_POST = {
